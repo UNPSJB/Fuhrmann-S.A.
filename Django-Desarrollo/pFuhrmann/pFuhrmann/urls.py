60e083428ea487c60427582b66680759d84f0af9
Matth <matthr.tw@gmail.com>
2014-09-11 23:31:43 -0300

    Django-Desarrollo-appWeb

---
+++ Django-Desarrollo/pFuhrmann/pFuhrmann/urls.py
@@ -0,0 +1,10 @@
+from django.conf.urls import patterns, include, url
+
+from django.contrib import admin
+admin.autodiscover()
+
+urlpatterns = patterns('',
+    # Examples:
+	url(r'^$','appWeb.views.index'),
+    url(r'^admin/', include(admin.site.urls)),
+)
