06ebc81866434f94613121dd69f8a3c0db7a94eb
Carla Santos <ccsjgs1990@gmail.com>
2014-09-15 17:18:03 -0300

    interfaz GProductor GLote

--- Django-Desarrollo/pFuhrmann/appWeb/views.py
+++ Django-Desarrollo/pFuhrmann/appWeb/views.py
@@ -3,3 +3,21 @@ from django.template import RequestContext
 
 def index (request):
 	return render_to_response('index.html', context_instance=RequestContext(request))
+
+def altaproductor (request):
+	return render_to_response('altaproductor.html', context_instance=RequestContext(request))
+
+def bajaproductor (request):
+	return render_to_response('bajaproductor.html', context_instance=RequestContext(request))
+
+def modificarproductor (request):
+	return render_to_response('modificarproductor.html', context_instance=RequestContext(request))
+
+def altalote (request):
+	return render_to_response('altalote.html', context_instance=RequestContext(request))
+
+def bajalote (request):
+	return render_to_response('bajalote.html', context_instance=RequestContext(request))
+
+def modificarlote (request):
+	return render_to_response('modificarlote.html', context_instance=RequestContext(request))
--- Django-Desarrollo/pFuhrmann/pFuhrmann/urls.py
+++ Django-Desarrollo/pFuhrmann/pFuhrmann/urls.py
@@ -6,5 +6,13 @@ admin.autodiscover()
 urlpatterns = patterns('',
     # Examples:
 	url(r'^$','appWeb.views.index'),
-    url(r'^admin/', include(admin.site.urls)),
+	url(r'^admin/', include(admin.site.urls)),
+
+    url(r'^altaproductor/', 'appWeb.views.altaproductor'),
+    url(r'^bajaproductor/', 'appWeb.views.bajaproductor'),
+    url(r'^modificarproductor/', 'appWeb.views.modificarproductor'),
+    url(r'^altalote/', 'appWeb.views.altalote'),
+    url(r'^bajalote/', 'appWeb.views.bajalote'),
+    url(r'^modificarlote/', 'appWeb.views.modificarlote'),
+
 )
