094cfe3f9fa262b11cc7619251f6ffa33c8b1d18
Matth <matthr.tw@gmail.com>
2014-09-18 18:46:39 -0300

    Models - Views

--- Django-Desarrollo/pFuhrmann/appWeb/views.py
+++ Django-Desarrollo/pFuhrmann/appWeb/views.py
@@ -1,5 +1,218 @@
 from django.shortcuts import render, render_to_response
 from django.template import RequestContext
+from appWeb.models import *
+from appWeb.forms import *
+from django.http import HttpResponse, HttpResponseRedirect
+from django.shortcuts import render_to_response, get_object_or_404
+from django.template import RequestContext
+from django.contrib.auth.models import User
+from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth.forms import AuthenticationForm
+from django.contrib.auth import login, authenticate, logout
+from django.contrib.auth.decorators import login_required
+
 
 def index (request):
 	return render_to_response('index.html', context_instance=RequestContext(request))
+
+
+# Funciones para crear formularios
+
+def nuevaCompra(request):
+	if request.method == 'POST':
+		formulario = compraForm(request.POST, request.FILES)
+		if formulario.is_valid():
+			formulario.save()
+			return HttpResponseRedirect('/compra')
+	else:
+		formulario = compraForm()
+	return render_to_response('compraForm.html', {'formulario':formulario}, context_instance=RequestContext(request))
+
+
+def nuevaVenta(request):
+	if request.method == 'POST':
+		formulario = ventaForm(request.POST, request.FILES)
+		if formulario.is_valid():
+			formulario.save()
+			return HttpResponseRedirect('/venta')
+	else:
+		formulario = ventaForm()
+	return render_to_response('ventaForm.html', {'formulario':formulario}, context_instance=RequestContext(request))
+
+def nuevaOrdenProduccion(request):
+	if request.method == 'POST':
+		formulario = nuevaOrdenProduccionForm(request.POST, request.FILES)
+		if formulario.is_valid():
+			formulario.save()
+			return HttpResponseRedirect('/nuevaOrdenProduccion')
+	else:
+		formulario = nuevaOrdenProduccionForm()
+	return render_to_response('nuevaOrdenProduccionForm.html', {'formulario':formulario}, context_instance=RequestContext(request))
+
+def modificarOrdenProduccion(request):
+	orden = OrdenProduccion.objects.all()
+	return render_to_response('modificarOrdenProduccion.html', {'lista':orden}, context_instance=RequestContext(request))
+	
+def modificarOrdenProduccionF(request):
+	if request.method == 'POST':
+		formulario = modificarOrdenProduccionForm(request.POST, request.FILES)
+		if formulario.is_valid():
+			formulario.save()
+			return HttpResponseRedirect('/modificarOrdenProduccionF')
+	else:
+		formulario = modificarOrdenProduccionForm()
+	return render_to_response('modificarOrdenProduccionForm.html', {'formulario':formulario}, context_instance=RequestContext(request))
+	
+def cancelarOrdenProduccion(request):
+	orden = OrdenProduccion.objects.all()
+	return render_to_response('cancelarOrdenProduccionForm.html', {'lista':orden}, context_instance=RequestContext(request))
+	
+def enviarFaseProduccion(request):
+	orden = OrdenProduccion.objects.all()
+	return render_to_response('enviarFaseProduccionForm.html', {'lista':orden}, context_instance=RequestContext(request))
+	
+def finalizarFaseProduccion(request):
+	orden = OrdenProduccion.objects.all()
+	return render_to_response('finalizarFaseProduccionForm.html', {'lista':orden}, context_instance=RequestContext(request))
+	
+def registrarLote(request):
+	if request.method == 'POST':
+		formulario = registrarLoteForm(request.POST, request.FILES)
+		if formulario.is_valid():
+			formulario.save()
+			return HttpResponseRedirect('/registrarLote')
+	else:
+		formulario = registrarLoteForm()
+	return render_to_response('registrarLoteForm.html', {'formulario':formulario}, context_instance=RequestContext(request))
+
+def modificarLote(request):
+	lote = Lote.objects.all()
+	return render_to_response('modificarLote.html', {'lista':lote}, context_instance=RequestContext(request))
+	
+def modificarLoteF(request):
+	if request.method == 'POST':
+		formulario = modificarLoteForm(request.POST, request.FILES)
+		if formulario.is_valid():
+			formulario.save()
+			return HttpResponseRedirect('/modificarLoteF')
+	else:
+		formulario = modificarLoteForm()
+	return render_to_response('modificarLoteForm.html', {'formulario':formulario}, context_instance=RequestContext(request))
+	
+
+def borrarLote(request):
+	lote = Lote.objects.all()
+	return render_to_response('borrarLoteForm.html', {'lista':lote}, context_instance=RequestContext(request))
+	
+def registrarFardo(request):
+	if request.method == 'POST':
+		formulario = registrarFardoForm(request.POST, request.FILES)
+		if formulario.is_valid():
+			formulario.save()
+			return HttpResponseRedirect('/registrarFardo')
+	else:
+		formulario = registrarFardoForm()
+	return render_to_response('registrarFardoForm.html', {'formulario':formulario}, context_instance=RequestContext(request))
+
+def modificarFardo(request):
+	fardo = Fardo.objects.all()
+	return render_to_response('modificarFardo.html', {'lista':fardo}, context_instance=RequestContext(request))
+	
+def modificarFardoF(request):
+	if request.method == 'POST':
+		formulario = modificarFardoForm(request.POST, request.FILES)
+		if formulario.is_valid():
+			formulario.save()
+			return HttpResponseRedirect('/modificarFardoF')
+	else:
+		formulario = modificarLoteForm()
+	return render_to_response('modificarFardoForm.html', {'formulario':formulario}, context_instance=RequestContext(request))
+	
+
+def registrarEstancia(request):
+	if request.method == 'POST':
+		formulario = registrarEstanciaForm(request.POST, request.FILES)
+		if formulario.is_valid():
+			formulario.save()
+			return HttpResponseRedirect('/registrarEstancia')
+	else:
+		formulario = registrarEstanciaForm()
+	return render_to_response('registrarEstanciaForm.html', {'formulario':formulario}, context_instance=RequestContext(request))
+
+def modificarEstancia(request):
+	estancia = Estancia.objects.all()
+	return render_to_response('modificarEstancia.html', {'lista':estancia}, context_instance=RequestContext(request))
+	
+def modificarEstanciaF(request):
+	if request.method == 'POST':
+		formulario = modificarEstanciaForm(request.POST, request.FILES)
+		if formulario.is_valid():
+			formulario.save()
+			return HttpResponseRedirect('/modificarEstanciaF')
+	else:
+		formulario = modificarEstanciaForm()
+	return render_to_response('modificarEstanciaForm.html', {'formulario':formulario}, context_instance=RequestContext(request))
+	
+def borrarEstancia(request):
+	estancia = Estancia.objects.all()
+	return render_to_response('borrarEstanciaForm.html', {'lista':estancia}, context_instance=RequestContext(request))
+	
+def registrarProductor(request):
+	if request.method == 'POST':
+		formulario = registrarProductorForm(request.POST, request.FILES)
+		if formulario.is_valid():
+			formulario.save()
+			return HttpResponseRedirect('/registrarProductor')
+	else:
+		formulario = registrarProductorForm()
+	return render_to_response('registrarProductorForm.html', {'formulario':formulario}, context_instance=RequestContext(request))
+
+def modificarProductor(request):
+	productores = Productor.objects.all()
+	return render_to_response('modificarProductor.html', {'lista':productores}, context_instance=RequestContext(request))
+	
+def modificarProductorF(request):
+	if request.method == 'POST':
+		formulario = modificarProductorForm(request.POST, request.FILES)
+		if formulario.is_valid():
+			formulario.save()
+			return HttpResponseRedirect('/modificarProductorF')
+	else:
+		formulario = modificarProductorForm()
+	return render_to_response('modificarProductorForm.html', {'formulario':formulario}, context_instance=RequestContext(request))
+	
+def borrarProductor(request):
+	productores = Productor.objects.all()
+	return render_to_response('borrarProductorForm.html', {'lista':productores}, context_instance=RequestContext(request))
+	
+
+def registrarRepresentante(request):
+	if request.method == 'POST':
+		formulario = registrarRepresentanteForm(request.POST, request.FILES)
+		if formulario.is_valid():
+			formulario.save()
+			return HttpResponseRedirect('/registrarRepresentante')
+	else:
+		formulario = registrarRepresentanteForm()
+	return render_to_response('registrarRepresentanteForm.html', {'formulario':formulario}, context_instance=RequestContext(request))
+
+def modificarRepresentante(request):
+	representantes = Representante.objects.all()
+	return render_to_response('modificarRepresentante.html', {'lista':representantes}, context_instance=RequestContext(request))
+	
+def modificarRepresentanteF(request):
+	if request.method == 'POST':
+		formulario = modificarRepresentanteForm(request.POST, request.FILES)
+		if formulario.is_valid():
+			formulario.save()
+			return HttpResponseRedirect('/modificarRepresentanteF')
+	else:
+		formulario = modificarRepresentanteForm()
+	return render_to_response('modificarRepresentanteForm.html', {'formulario':formulario}, context_instance=RequestContext(request))
+	
+def borrarRepresentante(request):
+	representantes = Representante.objects.all()
+	return render_to_response('borrarRepresentanteForm.html', {'lista':representantes}, context_instance=RequestContext(request))
+	
+
+
--- Django-Desarrollo/pFuhrmann/pFuhrmann/urls.py
+++ Django-Desarrollo/pFuhrmann/pFuhrmann/urls.py
@@ -1,10 +1,42 @@
 from django.conf.urls import patterns, include, url
-
 from django.contrib import admin
 admin.autodiscover()
 
 urlpatterns = patterns('',
-    # Examples:
+    
+	url(r'^index/','appWeb.views.index'),
 	url(r'^$','appWeb.views.index'),
+	
+    url(r'^admin/doc/', include('django.contrib.admindocs.urls')),
     url(r'^admin/', include(admin.site.urls)),
+    url(r'^compra/$', 'appWeb.views.nuevaCompra'),
+ 	url(r'^venta/$', 'appWeb.views.nuevaVenta'),
+    url(r'^nuevaOrdenProduccion/$', 'appWeb.views.nuevaOrdenProduccion'),
+    url(r'^modificarOrdenProduccion/$', 'appWeb.views.modificarOrdenProduccion'),
+    url(r'^cancelarOrdenProduccion/$', 'appWeb.views.cancelarOrdenProduccion'),
+    url(r'^enviarFaseProduccion/$', 'appWeb.views.enviarFaseProduccion'),
+    url(r'^finalizarFaseProduccion/$', 'appWeb.views.finalizarFaseProduccion'),
+  
+    url(r'^registrarLote/$', 'appWeb.views.registrarLote'),
+    url(r'^modificarLote/$', 'appWeb.views.modificarLote'),
+    url(r'^borrarLote/$', 'appWeb.views.borrarLote'),
+    url(r'^registrarFardo/$', 'appWeb.views.registrarFardo'),
+    url(r'^modificarFardo/$', 'appWeb.views.modificarFardo'),
+    url(r'^registrarEstancia/$', 'appWeb.views.registrarEstancia'),
+    url(r'^modificarEstancia/$', 'appWeb.views.modificarEstancia'),
+    url(r'^borrarEstancia/$', 'appWeb.views.borrarEstancia'),
+    url(r'^registrarProductor/$', 'appWeb.views.registrarProductor'),
+    url(r'^modificarProductor/$', 'appWeb.views.modificarProductor'),
+    url(r'^borrarProductor/$', 'appWeb.views.borrarProductor'),
+    url(r'^registrarRepresentante/$', 'appWeb.views.registrarRepresentante'),
+    url(r'^modificarRepresentante/$', 'appWeb.views.modificarRepresentante'),
+    url(r'^borrarRepresentante/$', 'appWeb.views.borrarRepresentante'),
+
+    #Url's de Forms para modificar Datos
+    url(r'^modificarOrdenProduccionF/$', 'appWeb.views.modificarOrdenProduccionF'),
+    url(r'^modificarLoteF/$', 'appWeb.views.modificarLoteF'),
+    url(r'^modificarFardoF/$', 'appWeb.views.modificarFardoF'),
+    url(r'^modificarEstanciaF/$', 'appWeb.views.modificarEstanciaF'),
+    url(r'^modificarProductorF/$', 'appWeb.views.modificarProductorF'),
+    url(r'^modificarRepresentanteF/$', 'appWeb.views.modificarRepresentanteF'),
 )
