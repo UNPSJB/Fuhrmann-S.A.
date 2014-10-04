e77e402801309afe81c40964669034f8baf1a7f2
Carla Santos <ccsjgs1990@gmail.com>
2014-10-04 17:17:47 -0300

    este anda urls.py

--- Django-Desarrollo/pFuhrmann/pFuhrmann/urls.py
+++ Django-Desarrollo/pFuhrmann/pFuhrmann/urls.py
@@ -10,30 +10,30 @@ urlpatterns = patterns('',
     url(r'^admin/doc/', include('django.contrib.admindocs.urls')),
     url(r'^admin/', include(admin.site.urls)),
 
-    """Registrar Operacion"""
+    #Registrar Operacion
     url(r'^compra/$', 'appWeb.views.nuevaCompra'),
  	url(r'^venta/$', 'appWeb.views.nuevaVenta'),
 
-    """Orden de produccion"""
+    #Orden de produccion
     url(r'^nuevaOrdenProduccion/$', 'appWeb.views.nuevaOrdenProduccion'),
     url(r'^modificarOrdenProduccion/$', 'appWeb.views.modificarOrdenProduccion'),
     url(r'^cancelarOrdenProduccion/$', 'appWeb.views.cancelarOrdenProduccion'),
     url(r'^enviarFaseProduccion/$', 'appWeb.views.enviarFaseProduccion'),
     url(r'^finalizarFaseProduccion/$', 'appWeb.views.finalizarFaseProduccion'),
     
-    """Lotes y Fardos"""
+    #Lotes y Fardos
     url(r'^registrarLote/$', 'appWeb.views.registrarLote'),
     url(r'^modificarLote/$', 'appWeb.views.modificarLote'),
     url(r'^borrarLote/$', 'appWeb.views.borrarLote'),
     url(r'^registrarFardo/$', 'appWeb.views.registrarFardo'),
     url(r'^modificarFardo/$', 'appWeb.views.modificarFardo'),
 
-    """Adm. Estancias"""
+    #Adm. Estancias
     url(r'^registrarEstancia/$', 'appWeb.views.registrarEstancia'),
     url(r'^modificarEstancia/$', 'appWeb.views.modificarEstancia'),
     url(r'^borrarEstancia/$', 'appWeb.views.borrarEstancia'),
 
-    """Adm. Personas"""
+    #Adm. Personas
     url(r'^registrarProductor/$', 'appWeb.views.registrarProductor'),
     url(r'^modificarProductor/$', 'appWeb.views.modificarProductor'),
     url(r'^borrarProductor/$', 'appWeb.views.borrarProductor'),
@@ -41,7 +41,7 @@ urlpatterns = patterns('',
     url(r'^modificarRepresentante/$', 'appWeb.views.modificarRepresentante'),
     url(r'^borrarRepresentante/$', 'appWeb.views.borrarRepresentante'),
 
-    """Adm. Maquinaria"""
+    #Adm. Maquinaria
     url(r'^registrarMaquinaria/$', 'appWeb.views.registrarMaquinaria'),
     url(r'^modificarMaquinaria/$', 'appWeb.views.modificarMaquinaria'),
     url(r'^borrarMaquinaria/$', 'appWeb.views.borrarMaquinaria'),
