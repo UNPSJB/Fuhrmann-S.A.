f89300e53151bcff1b9754639066de1ae7a7e53a
Carla Santos <ccsjgs1990@gmail.com>
2014-10-27 15:55:56 -0300

    inicio

--- Django-Desarrollo/pFuhrmann/appWeb/views.py
+++ Django-Desarrollo/pFuhrmann/appWeb/views.py
@@ -26,6 +26,8 @@ from django.contrib.auth.forms import AuthenticationForm
 def index (request):
     return render_to_response('index.html', context_instance=RequestContext(request))
 
+# --------------- Administracion de Usuario
+
 def nuevo_usuario(request):
     if request.method =='POST':
         formulario = UserCreationForm(request.POST)
@@ -36,6 +38,35 @@ def nuevo_usuario(request):
         formulario = UserCreationForm()
     return render_to_response('nuevousuario.html', {'formulario':formulario}, context_instance= RequestContext(request))
 
+def ingresar(request):
+    if not request.user.is_anonymous():
+            return HttpResponseRedirect('/privado')
+    if request.method =='POST':
+        formulario = AuthenticationForm(request.POST)
+        if formulario.is_valid:
+            usuario = request.POST ['username']
+            clave = request.POST ['password']
+            acceso = authenticate(username=usuario, password=clave)
+            if acceso is not None:
+                if acceso.is_active:
+                    login(request,acceso)
+                    return HttpResponseRedirect('/privado')
+                else:
+                    return render_to_response('noactivo.html',context_instance=RequestContext(request))
+            else:
+                return render_to_response('nousuario.html',context_instance=RequestContext(request))
+    else:
+        formulario = AuthenticationForm()
+    return render_to_response('ingresar.html',{'formulario':formulario}, context_instance=RequestContext(request))
+
+@login_required(login_url='/ingresar')
+def privado(request):
+    usuario = request.user
+    return render_to_response('privado.html',{'usuario':usuario},context_instance=RequestContext(request))
+
+def cerrar(request):
+    logout(request)
+    return HttpResponseRedirect('/')
 # --------------- Administracion de Compra
 
 def listadoCompra(request):
